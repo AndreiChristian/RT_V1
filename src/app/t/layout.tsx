b57a22d1675faa53ca6747dbbc66cac1edf5bdfd
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-10-18 10:56:19 +0300

    hello world

--- src/app/t/layout.tsx
+++ src/app/t/layout.tsx
@@ -1,9 +1,12 @@
-import { LayoutProps } from "../models/next_models";
+import ColorBoard from "@/components/ColorBoard/ColorBoard";
+import { LayoutProps } from "../_models/next_models";
 import EditingBar from "./_components/EditingBar";
 
 export default function Layout({ children }: LayoutProps) {
   return <>
     {children}
+    <ColorBoard />
     <EditingBar />
+    <h2>Here will be the color picker</h2>
   </>
 }
---
+++ src/components/ColorBoard/ColorBoard.tsx
@@ -0,0 +1,7 @@
+const array = new Array(10)
+
+export default function ColorBoard() {
+
+  return <div>
+  </div>
+}
